c5a0dd4b2788dbef5914e46f721c63b513b2c914
kidpixo <kidpixo@gmail.com>
2016-07-06 10:37:59 +0200

    added script to generate random note, now in gitignore.Not very useful in the repository.

---
+++ generate_random_notes.py
@@ -0,0 +1,31 @@
+import random
+import yaml
+
+tags_bag = ['spaces', 'bar' , 'test', 'foo', 'dog', 'python' , '42' , 'fish', 'Monty']
+
+n_notes = 20
+
+outtext = '''
+# Lorem ipsum dolor sit amet
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat suscipit auctor. Quisque accumsan ex in auctor dignissim. In id.
+
+## Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Suspendisse scelerisque nisl id risus accumsan pharetra.
+- Donec aliquet felis ut orci viverra, eu volutpat nisi eleifend.
+
+'''
+
+for i in range(n_notes):
+    ntags = random.randint(0,5)
+    filename = 'notes/notes %s.md' %i
+    tags_local = random.sample(tags_bag,ntags)
+    with open(filename, 'a') as file:
+        file.write('---\n')
+        file.write('tags : '+yaml.dump( tags_local))
+        file.write('---\n')
+        file.write(outtext)
+
+
