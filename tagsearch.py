7a13589b6bf85e5358c6ab16ae3f194a1b9e40e6
kidpixo <kidpixo@gmail.com>
2016-07-08 11:33:46 +0200

    handles no result in output

--- tagsearch.py
+++ tagsearch.py
@@ -7,14 +7,15 @@ Tags with ! prepended are delete from result.
 Usage:
   tagsearch.py
   tagsearch.py [<tags>...]
-  tagsearch.py [-fnd] [-o | -ol] [<tags>...]
+  tagsearch.py [-fnd] [-p | -pl] [<tags>...]
+  tagsearch.py [-t | -p] [<tags>...]
   tagsearch.py -h | --help
   tagsearch.py -v | --version
 
 Options:
   -f --fullpath  output full file path (default fullpath).
   -n --noalign   aligh the output in columns (default align).
-  -o --onlypath  output only the matching filenames, flat list ad string (or list if -l is set).
+  -p --pathonly  output only the matching filenames, flat list ad string (or list if -l is set).
   -l --list      if -o is set, switch to list (\\n separated) output instead flat list.
   -d --debug     print the command to execute in the command line and the passed arguments and do not execute the actual command.
   -h --help      show help.
@@ -29,19 +30,23 @@ import yaml
 def extract_tags(cmd, basepath_len, arguments):
     pro = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     data = pro.communicate()[0].split('\n')
-    if arguments['--fullpath']:
-        dict_data = {yaml.load(d).keys()[0]: yaml.load(d)[yaml.load(d).keys()[0]] for d in data if d is not ''}
+
+    if not (len(data) == 1) or (bool(data[0])):
+        if arguments['--fullpath']:
+            dict_data = {yaml.load(d).keys()[0]: yaml.load(d)[yaml.load(d).keys()[0]] for d in data if d is not ''}
+        else:
+            dict_data = {yaml.load(d).keys()[0][basepath_len:]: yaml.load(d)[yaml.load(d).keys()[0]] for d in data if d is not ''}
     else:
-        dict_data = {yaml.load(d).keys()[0][basepath_len:]: yaml.load(d)[yaml.load(d).keys()[0]] for d in data if d is not ''}
+        dict_data = {}
 
     return dict_data
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='tagsearch 0.1')
-    # this handle cases like $HOME in the basepath.
+    # This handle cases like $HOME in the basepath.
     # I usually put the folder somewhere (drive, dropbox) and symlynk in my
-    # home. this way, one path rules them all.
+    # home. This way, one path rules them all.
     # basepath_encoded = '$HOME/.notes/'
     # pro = subprocess.Popen('echo '+basepath_encoded, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # basepath = pro.communicate()[0].split('\n')[0]
@@ -51,17 +56,11 @@ if __name__ == '__main__':
     tmp_cmd = " grep '^\s*tags :' %s*.md | sed -E -e 's/:tags//' " % (basepath)
 
     if len(arguments['<tags>']) != 0:
-        for ar in arguments['<tags>']:
-            if ar[0] == "!":
-                # delete the occurence of a tag
-                tmp_cmd = tmp_cmd + " -e '/^.* : .*"+ar[1:]+".*/d'"
-            else:
-                # preserve the occurence of a tag
-                tmp_cmd = tmp_cmd + " -e '/^.* : .*"+ar+".*/!d'"
+        tmp_cmd = tmp_cmd + ''.join([" -e '/^.* : .*"+ar[1:]+".*/d'" if ar[0] == "!" else " -e '/^.* : .*"+ar+".*/!d'" for ar in arguments['<tags>']])
 
     if arguments['--debug']:
-        print('command to execute:')
-        print(tmp_cmd)
+        print('------------------')
+        print('command to execute:', tmp_cmd)
         print('------------------')
         print('arguments:')
         print(arguments)
@@ -70,15 +69,15 @@ if __name__ == '__main__':
     else:
 
         dict_data = extract_tags(tmp_cmd, len(basepath), arguments)
-
-        if not arguments['--onlypath']:
-            if not arguments['--noalign']:
-                max_keys_len = len(max(dict_data, key=len))
-                print('\n'.join('{:<{}s} : {}'.format(k.encode('utf-8'), max_keys_len, v) for k, v in dict_data.iteritems()))
-            else:
-                print('\n'.join('{:<s} : {}'.format(k.encode('utf-8'), v) for k, v in dict_data.iteritems()))
-        else:
-            if not arguments['--list']:
-                print(' '.join(dict_data.keys()))
-            else:
-                print('\n'.join(dict_data.keys()))
+        if len(dict_data) != 0:               # not results in output
+            if not arguments['--pathonly']:       # only path == True
+                if not arguments['--noalign']:    # noalign == True
+                    max_keys_len = len(max(dict_data, key=len))
+                    print('\n'.join('{:<{}s} : {}'.format(k.encode('utf-8'), max_keys_len, v) for k, v in dict_data.iteritems()))
+                else:                             # noalign == True
+                    print('\n'.join('{:<s} : {}'.format(k.encode('utf-8'), v) for k, v in dict_data.iteritems()))
+            else:                                 # only path == False
+                if not arguments['--list']:       # list == True
+                    print(' '.join(dict_data.keys()))
+                else:                             # list == False
+                    print('\n'.join(dict_data.keys()))
