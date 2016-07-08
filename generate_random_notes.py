d056205147b5645547d19c302cf7cd1d21c89389
kidpixo <kidpixo@gmail.com>
2016-07-08 11:34:23 +0200

    correct generate_random_notes PEP8 errors

--- generate_random_notes.py
+++ generate_random_notes.py
@@ -1,31 +1,29 @@
 import random
 import yaml
 
-tags_bag = ['spaces', 'bar' , 'test', 'foo', 'dog', 'python' , '42' , 'fish', 'Monty']
+tags_bag = ['spaces',  'bar',  'test',  'foo',  'dog',  'python',  '42',  'fish',  'Monty']
 
-n_notes = 100
+n_notes = 50
 
 outtext = '''
 # Lorem ipsum dolor sit amet
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat suscipit auctor. Quisque accumsan ex in auctor dignissim. In id.
+Lorem ipsum dolor sit amet,  consectetur adipiscing elit. Fusce consequat suscipit auctor. Quisque accumsan ex in auctor dignissim. In id.
 
 ## Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
 
-- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet,  consectetur adipiscing elit.
 - Suspendisse scelerisque nisl id risus accumsan pharetra.
-- Donec aliquet felis ut orci viverra, eu volutpat nisi eleifend.
+- Donec aliquet felis ut orci viverra,  eu volutpat nisi eleifend.
 
 '''
 
 for i in range(n_notes):
-    ntags = random.randint(0,5)
-    filename = 'notes/notes %s.md' %i
-    tags_local = random.sample(tags_bag,ntags)
-    with open(filename, 'w') as file:
+    ntags = random.randint(0, 5)
+    filename = 'notes/notes %s.md' % i
+    tags_local = random.sample(tags_bag, ntags)
+    with open(filename,  'w') as file:
         file.write('---\n')
-        file.write('tags : '+yaml.dump( tags_local))
+        file.write('tags : '+yaml.dump(tags_local))
         file.write('---\n')
         file.write(outtext)
-
-
